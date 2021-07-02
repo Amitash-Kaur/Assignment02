d878182013d7af5bb36b25da2f61f9ae9fc5524f
Amitash <Amitash@AMITASH>
2021-07-01 22:43:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleSolver triangle = new TriangleSolver();
+
+            int input = 0;
+            bool success;
+            int choice = 0;
+
+            do
+            {
+                Console.WriteLine("\nPlease select an option from below mentioned list : ");
+                Console.WriteLine("1. Please enter the dimesions of triangle: ");
+                Console.WriteLine("2. Exit \n");
+
+                //user choice inpiut
+                try
+                {
+                    do
+                    {
+                        success = int.TryParse(Console.ReadLine(), out choice);
+                        if (!success)
+                        {
+                            Console.WriteLine("Invalid input ");
+                            Console.WriteLine("Please try another input !! ");
+                            continue;
+                        }
+
+                    }
+
+                    while (!success);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                switch (choice)
+                {
+
+                    case 1:
+
+                        //Side 1
+                        Console.Write("\n");
+                        Console.Write("Please enter a value greater than zero for side 1 of the triangle: ");
+
+                        try
+                        {
+                            do
+                            {
+                                success = int.TryParse(Console.ReadLine(), out input);
+                                if (!success)
+                                {
+                                    Console.WriteLine("Invalid input ");
+                                    Console.WriteLine("Please try another input !! ");
+                                    continue;
+                                }
+                                if (input <= 0)
+                                {
+                                    success = false;
+                                    Console.WriteLine("Value should be greater than zero (0) ");
+                                    Console.WriteLine("Please try another input !! ");
+                                }
+
+                            }
+
+                            while (!success);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        triangle.SetSide1(input);
+
+                        //Side 2
+                        Console.Write("Please enter a value greater than zero for Side 2 of the triangle: ");
+                        try
+                        {
+                            do
+                            {
+                                success = int.TryParse(Console.ReadLine(), out input);
+                                if (!success)
+                                {
+                                    Console.WriteLine("Invalid input ");
+                                    Console.WriteLine("Please try another input !! ");
+                                    continue;
+                                }
+                                if (input <= 0)
+                                {
+                                    success = false;
+                                    Console.WriteLine("Value should be greater than zero (0) ");
+                                    Console.WriteLine("Please try another input !! ");
+                                }
+
+                            }
+
+                            while (!success);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        triangle.SetSide2(input);
+
+                        //Side 3
+                        Console.Write("Please enter a value greater than zero for Side 3 of the triangle: ");
+                        try
+                        {
+                            do
+                            {
+                                success = int.TryParse(Console.ReadLine(), out input);
+                                if (!success)
+                                {
+                                    Console.WriteLine("Invalid input ");
+                                    Console.WriteLine("Please try another input !! ");
+                                    continue;
+                                }
+                                if (input <= 0)
+                                {
+                                    success = false;
+                                    Console.WriteLine("Value should be greater than zero (0) ");
+                                    Console.WriteLine("Please try another input !! ");
+                                }
+
+                            }
+
+                            while (!success);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        triangle.SetSide3(input);
+
+                        Console.WriteLine(" {0} \n ", triangle.TriangleCategory);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Exitting ");
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter valid value from menu options (1/2): ");
+                        continue;
+                }
+            }
+            while (choice != 2);
+        }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment02
+{
+    public class TriangleSolver
+    {
+
+        private int Side1;
+        private int Side2;
+        private int Side3;
+        int Equi;
+        int Isos;
+        int Scal;
+        int yes;
+
+        // Constructor1; default values are 1
+        public TriangleSolver()
+        {
+            this.Side1 = 1;
+            this.Side2 = 1;
+            this.Side3 = 1;
+        }
+
+        // Constructor2 to set Side1, Side2, Side3 for values inputted by the user
+        public TriangleSolver(int Side1, int Side2, int Side3)
+        {
+            this.Side1 = Side1;
+            this.Side2 = Side2;
+            this.Side3 = Side3;
+        }
+
+        //to analyze category of traingle
+        public int TriangleCategory
+        {
+            get
+            {
+                if (Side1 == Side2 && Side2 == Side3)
+                {
+                    Console.Write("\n");
+                    Console.Write("Values entered by user forms a triangle. \n",yes);
+                    Console.Write("This is an Equilateral Triangle.\n", Equi);
+                    return Equi;
+                    
+                }
+                else if (Side1 == Side2 || Side1 == Side3 || Side2 == Side3)
+                {
+                    Console.Write("\n");
+                    Console.Write("Values entered by user forms a triangle. \n");
+                    Console.Write("This is an Isosceles Triangle.\n",Isos);
+                    return Isos;
+                }
+                else
+                {
+                    Console.Write("\n");
+                    Console.Write("Values entered by user forms a triangle. \n");
+                    Console.Write("This is a Scalene Triangle.\n",Scal);
+                    return Scal;
+
+                }
+            }
+        }
+
+        //contains a static method called Analyze which accepts three integers as an input, returns a string as an output.
+        //public static int Analyze()
+        //{
+        //    return ();
+        //}
+
+
+        public void SetSide1(int Side1)
+        {
+            this.Side1 = Side1;
+            return;
+        }
+
+        public void SetSide2(int Side2)
+        {
+            this.Side2 = Side2;
+            return;
+        }
+        
+        public void SetSide3(int Side3)
+        {
+            this.Side3 = Side3;
+            return;
+        }
+
+        //public int GetSide1()
+        //{
+        //    return this.Side1;
+        //}
+
+        //public int GetSide2()
+        //{
+        //    return this.Side2;
+        //}
+
+        //public int GetSide3()
+        //{
+        //    return this.Side3;
+        //}
+
+    }
+}
